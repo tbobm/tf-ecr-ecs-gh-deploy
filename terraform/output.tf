526039354edb03d017e05d67abc2f3d1684cf097
Theo Bob Massard <tbobm@protonmail.com>
2021-03-29 14:18:16 +0200

    feat(output): add outputs for Github Actions
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- terraform/output.tf
+++ terraform/output.tf
@@ -14,7 +14,32 @@ output "ecr_url" {
   description = "The ECR repository URL"
 }
 
+output "ecr_repository_name" {
+  value       = aws_ecr_repository.repository.name
+  description = "The ECR repository name"
+}
+
+output "aws_region" {
+  value       = local.region
+  description = "The AWS region used"
+}
+
 output "app_url" {
   value       = aws_lb.alb.dns_name
   description = "The public ALB DNS"
+}
+
+output "ecs_cluster" {
+  value       = aws_ecs_cluster.cluster.name
+  description = "The ECS cluster name"
+}
+
+output "ecs_service" {
+  value       = aws_ecs_service.service.name
+  description = "The ECS service name"
+}
+
+output "container_name" {
+  value       = local.container.name
+  description = "Container name for the ECS task"
 }
